81913ab1507f46b3743cf4aecebba8ee235467a6
juan <m.c.juandeanda@gmail.com>
2020-08-12 01:04:37 -0500

    v110820

--- pySPH_Shocktube/kernelhGauss.cpp
+++
@@ -1,8 +0,0 @@
-#include "kernelhGauss.hpp"
-
-double kernelhGauss::wij(double xi, double xj, double h){
-  return (1/((sqrt(pi)*h)))*(exp(-1.0*pow(xi-xj,2)/pow(h,2))*(1.5-(pow(xi-xj,2)/pow(h,2))));
- }
- double kernelhGauss::dwij(double xi, double xj, double h){
-  return (1/((sqrt(pi)*pow(h,5))))*(exp(-1.0*pow(xi-xj,2)/pow(h,2))*((2*pow(xi-xj,3))-(5*pow(h,2)*(xi-xj))));
- }
--- setup.py
+++ setup.py
@@ -1,20 +1,25 @@
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
+import setuptools
 
-setup(
-name='pySPH-ChockTube',
-   version='1.0',
-   description='A useful module',
-   author='Man Foo',
-   author_email='m.c.juandeanda@gmail.com',
-   packages=[ 'pySPH_chocktube'],
-   package_dir = {'pySPH_Shocktube': 'pySPH_Shocktube'},
-   ext_modules=[Extension("pySPH_Shocktube.pyParticle",
-                             ["pySPH_Shocktube/pyParticle.pyx",
-                              "pySPH_Shocktube/Particle.cpp"], language="c++"),
-                Extension("pySPH_Shocktube.pykernelhGauss",
-                             ["pySPH_Shocktube/pykernelhGauss.pyx",
-                              "pySPH_Shocktube/kernelhGauss.cpp"], language="c++")],
-   cmdclass = {'build_ext': build_ext}
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pySEED",
+    version="1.0",
+    author="Juan de Anda Suarez",
+    author_email="m.c.juandeanda@gmail.com",
+    description="",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="https://github.com/juandeanda/pySPH_Shocktube.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5.2',
+    install_requires=[
+   'numpy>=1.18.2',
+   ]
 )
